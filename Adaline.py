16b94dc60ce3375d165ecfdcf9a529165a6b1f4c
Yuval sandler <sandler2806@gmai.com>
2022-04-10 15:30:46 +0300

    print

--- Adaline.py
+++ Adaline.py
@@ -10,33 +10,34 @@ class Adaline:
     :param condition: T2
     :return: opencv solution, my implementation
     """
-    def __init__(self , jsonfile:str , condition:bool):
+
+    def __init__(self, jsonfile: str, condition: bool):
         # class member points which is a list of points. each point is a tuple ->(x,y, value 1 or -1)
         self.points = []
         # read the points from the json file
         jsonfile = "" + str(jsonfile) + ".json"
         if jsonfile is not None:
-            with open(jsonfile , 'r') as jsonfile:
-                json_object =  json.load(jsonfile)
+            with open(jsonfile, 'r') as jsonfile:
+                json_object = json.load(jsonfile)
                 jsonfile.close()
                 for i in range(len(json_object)):
                     p = json_object[str(i)]
                     if condition:
                         if p['y'] > 1:
-                            single_point = (p['x'] , p['y'] , 1)
+                            single_point = (p['x'], p['y'], 1)
                         else:
                             single_point = (p['x'], p['y'], -1)
                         self.points.append(single_point)
 
 
-
 def saveDataToCSV():
+    print()
 
 
 def train(DataSet: list):
     w1 = w2 = b = alpha = float("{0:.3f}".format(np.random.rand()))
     mseTotal = []
-    table=[]
+    table = []
     while True:
         delta = []
         mse = []
@@ -55,8 +56,10 @@ def train(DataSet: list):
         if mseTotal[-1] < 10:
             break
 
-def test(DataSet:list):
+
+def test(DataSet: list):
+    print()
 
 
 if __name__ == '__main__':
-    data = Adaline("dataSets/dataSet1" , True)
+    data = Adaline("dataSets/dataSet1", True)
